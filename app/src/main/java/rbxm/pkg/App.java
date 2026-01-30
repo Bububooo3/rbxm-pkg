cae55c4332de24bc4b243974fab6861bf01ec34e
Retroid <bububooo3@gmail.com>
2026-01-30 06:45:23 -0500

    a

--- app/src/main/java/rbxm/pkg/App.java
+++ app/src/main/java/rbxm/pkg/App.java
@@ -1,13 +1,28 @@
 package rbxm.pkg;
 
+import com.google.gson.Gson;
+import com.sun.tools.javac.Main;
+
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+
+import com.google.common.reflect.TypeToken;
+
 public class App {
     private static Registry registry;
 
     public static void main(String[] args) throws Exception {
-        
+        loadRegistry();
     }
 
     private static void loadRegistry() throws Exception {
-        Gson 
+        Gson gson = new Gson();
+        Type type = new TypeToken<Registry>() {
+        }.getType();
+
+        try (InputStreamReader reader = new InputStreamReader(
+                Main.class.getResourceAsStream("https://bububooo3.github.io/rbxm-pkg/registry.json"))) {
+            registry = gson.fromJson(reader, type);
+        }
     }
 }
